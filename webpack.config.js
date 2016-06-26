d8ebdab2e703eed57d2f11ed6dfba2f99d0ee18c
Yuta Shimakawa <banana.umai@gmail.com>
2016-06-26 16:12:57 +0900

    babelのシンプルな実行例

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: `${__dirname}/app.js`,
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
